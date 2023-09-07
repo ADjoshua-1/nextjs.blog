bc7aa2ea91494d910cac4e8642395960090aa1d3
ADjoshua-1 <ajt-simon.student@ua.edu.ph>
2023-09-07 23:48:22 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I’m Adrian Joshua T. Simon my hobbies is playing basketball, my favourite
+music is ”tuloy pa rin” , my favourite food is adobo and I’m a shy person.</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
